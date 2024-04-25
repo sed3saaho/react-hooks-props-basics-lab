206b3980b4731e5e04283b1f2f98dec77335d310
Sedly <sedly.keith@student.moringaschool.com>
2024-04-25 09:19:30 +0300

    react-hooks-props-basics-lab

--- src/components/About.js
+++ src/components/About.js
@@ -1,12 +1,15 @@
 import React from "react";
+import Links from "./Links";
+import user from "../data/user";
 
 function About() {
   return (
     <div id="about">
       <h2>About Me</h2>
-      <p>Put the bio in here</p>
+      <p>{user.bio}</p>
       <img src="https://i.imgur.com/mV8PQxj.gif" alt="I made this" />
-      {/* add your <Links /> component here */}
+      {/* add your <Links /> component here */
+      <Links github={user.links.github} linkedin={user.links.linkedin} />}
     </div>
   );
 }
